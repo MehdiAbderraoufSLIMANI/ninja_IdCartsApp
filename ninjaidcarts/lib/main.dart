c392c7e01168057017f43f0a2151bf71b4ce8f42
justmahdii <didoslimani10@gmail.com>
2022-12-21 13:55:28 +0100

    creating the appbar

--- ninjaidcarts/lib/main.dart
+++ ninjaidcarts/lib/main.dart
@@ -9,8 +9,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("hello world"),
+    return const MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text('ID cart')),
+        backgroundColor: const Color.fromARGB(255, 104, 104, 104),
+      ),
     );
   }
 }
